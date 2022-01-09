6759b1c009bfc7d1b6484724faef2dfdd7fecc0f
Sufgan <68510282+Sufgan@users.noreply.github.com>
2022-01-09 12:35:26 +0300

    акселерометр почему-то не работающая

--- app/src/main/java/com/stars/paint/MainActivity.java
+++ app/src/main/java/com/stars/paint/MainActivity.java
@@ -2,9 +2,13 @@ package com.stars.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView one, two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
