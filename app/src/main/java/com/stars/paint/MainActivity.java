f824c502168222c0d2353a8e39b45053300aa95e
Sufgan <68510282+Sufgan@users.noreply.github.com>
2022-01-09 14:51:44 +0300

    cursor

--- app/src/main/java/com/stars/paint/MainActivity.java
+++ app/src/main/java/com/stars/paint/MainActivity.java
@@ -7,22 +7,19 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
-    public static TextView one, two;
-
-    private SensorManager sensorManager;
+    public ImageView cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-
-        one = findViewById(R.id.textView);
-        two = findViewById(R.id.textView2);
+        cursor = findViewById(R.id.cursor);
 
         SensorData sensData = new SensorData(this);
     }
